e71e1ff515cd289035a420ef0e2b979ecb68bc0d
Wolfgang Faust <wolfgang42@git>
2017-02-23 11:43:12 -0500

    Fix various problems revealed in IE/Edge.
    Only tested in IE 11, though.

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 (function() {
+	function radioValue(elements) {
+		for (var i = 0; i < elements.length; i++) {
+			if (elements[i].checked) return elements[i].value
+		}
+	}
 	var form = document.getElementById('control-form')
 	var indentIndication = document.getElementById('indent-indication')
 	function updateDisplay() {
 		['literal', 'folded', 'clip', 'strip', 'keep', 'indicate-indent'].forEach(function(cls) {
 			document.body.classList.remove('yaml-'+cls)
 		})
-		document.body.classList.add('yaml-'+form.elements.chomping.value)
-		document.body.classList.add('yaml-'+form.elements.blockstyle.value)
+		document.body.classList.add('yaml-'+radioValue(form.elements.chomping))
+		document.body.classList.add('yaml-'+radioValue(form.elements.blockstyle))
 		if (form.elements.indicateIndent.checked) {
 			document.body.classList.add('yaml-indicate-indent')
 		}
@@ -17,6 +22,7 @@
 			indents[i].innerHTML = indentation
 		}
 	}
+	window.updateYamlDisplay = updateDisplay
 	form.addEventListener('change', updateDisplay)
 	updateDisplay()
 })()
